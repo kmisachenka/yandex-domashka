7ead4b8f23475895d8c0ba11f6a88885bfe1210b
Kiryl Misachenka <kmisachenka@gmail.com>
2019-04-19 17:53:34 +0300

    Поменял название теста на генерацию ID

--- backend/src/utils/id.test.ts
+++ backend/src/utils/id.test.ts
@@ -4,7 +4,7 @@ Date.now = jest.fn(() => 123456789);
 
 describe('ID', () => {
   describe('generate()', () => {
-    it('should return zero as first value', () => {
+    it('should return a correct id', () => {
       expect(generate()).toEqual(123456789);
     });
   });
