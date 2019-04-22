f7bef40e058012d5f912ea1247ca21528552dbbf
Kiryl Misachenka <kmisachenka@gmail.com>
2019-04-22 17:33:07 +0300

    Поправил опечатки в названиях e2e тестов

--- frontend/cypress/integration/app.spec.js
+++ frontend/cypress/integration/app.spec.js
@@ -5,12 +5,12 @@ describe('Yandex Domashka', () => {
       cy.visit('http://localhost:9000');
       cy.contains('Заметки');
     });
-    it("don't apper on touch", () => {
+    it("don't appear on touch", () => {
       cy.viewport(768, 946);
       cy.visit('http://localhost:9000');
       cy.contains('Заметки').not();
     });
-    it("don't apper on mobile", () => {
+    it("don't appear on mobile", () => {
       cy.viewport(340, 480);
       cy.visit('http://localhost:9000');
       cy.contains('Заметки').not();
